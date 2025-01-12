5fdcca0a8b30bf0996900d8fc6590d042be58187
brady <no@email.com>
2025-01-11 18:02:22 -0800

    Initial commit.

---
+++ src/tablist/tablist.js
@@ -0,0 +1,28 @@
+function getCurrentWindowTabs() {
+    return browser.tabs.query({ currentWindow: true });
+  }
+  
+function listTabs() {
+    getCurrentWindowTabs().then((tabs) => {
+        const tabsListElement = document.getElementById('tabs-list-popup');
+        let tabList = [];
+
+        for (const tab of tabs) {
+            tabList.push(tab.url)
+        }
+
+        tabsListElement.textContent = tabList.join('\n');
+    })
+}
+
+function addToClipboard() {
+    var text = document.getElementById('tabs-list-popup').textContent;
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Text copied to clipboard from TabList.');
+    }).catch(function(error) {
+        console.error('Error copying text: ', error);
+    });
+}
+
+document.getElementById('copy-button').addEventListener('click', addToClipboard);
+document.addEventListener("DOMContentLoaded", listTabs);
